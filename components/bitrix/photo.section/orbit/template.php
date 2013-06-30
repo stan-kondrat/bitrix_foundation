290196bff9e822c88eacac3523183b702c4df463
Stan Kondrat <stan.kondrat gmail com>
2013-07-01 02:19:06 +0700

    Add 'top-bar' and 'orbit' components templates

---
+++ components/bitrix/photo.section/orbit/template.php
@@ -0,0 +1,28 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
+
+
+
+<div class="slideshow-wrapper">
+  <div class="preloader"></div>
+	<ul data-orbit>
+
+<?foreach($arResult["ROWS"] as $arItems):?>
+	<?foreach($arItems as $arItem):?>
+		<?if(is_array($arItem)):?>
+			<li>
+			<?
+			$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+			$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BPS_ELEMENT_DELETE_CONFIRM')));
+			?>
+				<?if(is_array($arItem["PICTURE"])):?>
+					<img border="0" src="<?=$arItem["DETAIL_PICTURE"]["SRC"]?>" width="<?=$arItem["DETAIL_PICTURE"]["WIDTH"]?>" height="<?=$arItem["PICTURE"]["HEIGHT"]?>" alt="<?=$arItem["NAME"]?>" title="<?=$arItem["NAME"]?>" /><br />
+				<?endif?>
+				<div class="orbit-caption"><?=$arItem["NAME"]?></div>
+			</li>
+		<?endif;?>
+	<?endforeach?>
+<?endforeach?>
+</ul>
+</div>
+
---
+++ footer.php
@@ -0,0 +1,13 @@
+    </div>
+  </div>
+
+  
+  <script src="<?=SITE_TEMPLATE_PATH?>/javascripts/foundation/foundation.js"></script>
+  <script src="<?=SITE_TEMPLATE_PATH?>/javascripts/foundation/foundation.topbar.js"></script>
+  <script src="<?=SITE_TEMPLATE_PATH?>/javascripts/foundation/foundation.orbit.js"></script>
+
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>

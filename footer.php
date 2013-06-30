3ca680722c769bfb446d5d706bb8c01d85a11a25
Stan Kondrat <stan.kondrat gmail com>
2013-07-01 03:37:18 +0700

    Enable reveal

--- footer.php
+++ footer.php
@@ -7,6 +7,7 @@
   <script src="<?=SITE_TEMPLATE_PATH?>/javascripts/foundation/foundation.orbit.js"></script>
   <script src="<?=SITE_TEMPLATE_PATH?>/javascripts/foundation/foundation.clearing.js"></script>
   <script src="<?=SITE_TEMPLATE_PATH?>/javascripts/foundation/foundation.magellan.js"></script>
+  <script src="<?=SITE_TEMPLATE_PATH?>/javascripts/foundation/foundation.reveal.js"></script>
 
   <script>
     $(document).foundation();
